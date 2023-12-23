0cf6c1fce0948f190fc8177d8bb87fc5ca5d6b88
Khachatur <47743140+Khachdallak02@users.noreply.github.com>
2023-12-23 19:33:18 +0400

    Mostly Adding and updating addresses

--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,4 @@
+import csv
 from functools import partial
 from asyncio import get_running_loop
 from shutil import rmtree
@@ -6,10 +7,13 @@ import logging
 import os
 from typing import Union, Optional
 from dotenv import load_dotenv
-from telethon import TelegramClient
+from telethon import TelegramClient, Button, events, types
 from telethon.events import NewMessage, StopPropagation
 from telethon.tl.custom import Message
-
+from telethon.tl.functions.messages import SendMessageRequest
+from telethon.tl.types import InputPeerUser, ReplyKeyboardForceReply
+import pandas as pd
+import re
 from utils import download_files, add_to_zip
 
 load_dotenv()
@@ -38,6 +42,26 @@ bot = TelegramClient(
     'quick-zip-bot', api_id=API_ID, api_hash=API_HASH
 ).start(bot_token=BOT_TOKEN)
 
+
+def is_valid_usdt_bep20_address(address):
+    """
+    Validate whether the input string is a USDT address in BEP20 (Binance Smart Chain) format.
+
+    BEP20 addresses should follow the Ethereum address format, as they are Ethereum-style addresses.
+    They typically start with '0x' followed by 40 hexadecimal characters. This function checks
+    for that specific pattern.
+
+    Args:
+    address (str): The address string to be validated.
+
+    Returns:
+    bool: True if the address is in valid BEP20 format, False otherwise.
+    """
+    # Regex pattern for a valid Ethereum/BEP20 address
+    pattern = r"^0x[a-fA-F0-9]{40}$"
+
+    # Check if the address matches the pattern
+    return bool(re.match(pattern, address))
 
 @bot.on(NewMessage(pattern='/add'))
 async def start_task_handler(event: MessageEvent):
@@ -110,6 +134,80 @@ async def zip_handler(event: MessageEvent):
         tasks.pop(event.sender_id)
 
     raise StopPropagation
+
+@bot.on(NewMessage(pattern='/crypto_address'))
+async def handle_crypto_address(event):
+    user_id = event.sender_id
+    username = event.sender.username
+    current_address = None
+
+    try:
+        with open('../data/crypto_addresses.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row['user_id'] == str(user_id):
+                    current_address = row['address']
+                    break
+    except (FileNotFoundError, StopIteration):
+        current_address = None
+
+    if current_address:
+        message = f"Current USDT Address (BEP20 network): {current_address}\nIf you want to update it send "\
+                  "your new address (nothing else, only the address). Tutorial for finding your USDT address in Binance: "\
+                  "https://www.youtube.com/watch?v=bSU84swL5kw&t=1s"
+        await bot.send_message(
+            event.sender_id,
+            message,
+
+            buttons=[
+                Button.url("Tutorial for finding your USDT address in Binance",
+                           url="https://www.youtube.com/watch?v=bSU84swL5kw&t=1s"),
+                Button.inline("USDT Address (BEP20 network)", data=current_address)
+
+            ]
+
+        )
+
+    else:
+        message = "⚠️ No crypto address found. Please set one for receiving payments. " \
+
+        await event.respond(
+            message,
+            buttons=[
+                Button.url("Tutorial for finding your USDT address in Binance",
+                           url="https://www.youtube.com/watch?v=bSU84swL5kw&t=1s")
+            ],
+            link_preview=False
+        )
+        force_reply = ReplyKeyboardForceReply(single_use=True, selective=True)
+        await bot(SendMessageRequest(
+            peer=await event.get_input_chat(),
+            message="💬 Please reply to this message with your USDT address (BEP20 network) (only the address):",
+            reply_markup=force_reply,
+
+        ))
+
+
+    @bot.on(NewMessage(from_users=event.sender_id, func=lambda e: e.text.strip().startswith('0x')))
+    async def wait_for_reply(reply_event):
+        new_address = reply_event.text.strip()
+        if is_valid_usdt_bep20_address(new_address):
+            try:
+                with open('../data/crypto_addresses.csv', 'r') as file:
+                    reader = csv.reader(file)
+                    for row in reader:
+                        if row['user_id'] == str(user_id):
+                            row['address'] = new_address
+                            break
+            except (FileNotFoundError, StopIteration):
+                with open('../data/crypto_addresses.csv', 'w') as file:
+                    writer = csv.writer(file)
+                    writer.writerow([user_id, username, new_address])
+            await reply_event.reply("Crypto address updated.")
+        else:
+            await reply_event.reply("Invalid address. Make sure you chose the correct <b>BEP20 network</b>."
+                                    " Run the /crypto_address command to try again.", parse_mode='html')
+        bot.remove_event_handler(wait_for_reply)
 
 
 @bot.on(NewMessage(pattern='/cancel'))
